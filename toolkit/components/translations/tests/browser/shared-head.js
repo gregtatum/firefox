2bab00b2a3227ed098d24f30dc5b64a2718f0107
Erik Nordin <nordzilla@mozilla.com>
2025-05-20 13:18:22 -0500

    Bug 1820252 - Add FindBar tests r=#translations-reviewers
    This patch adds new Full-Page Translations tests that ensure
    showing and hiding the FindBar correctly transitions the
    TranslationsDocument between lazy and content-eager mode,
    only on a per-tab basis.
    
    Differential Revision: https://phabricator.services.mozilla.com/D250000

--- toolkit/components/translations/tests/browser/shared-head.js
+++ toolkit/components/translations/tests/browser/shared-head.js
@@ -1598,9 +1598,9 @@ async function captureTranslationsError(callback) {
   return errors;
 }
 
-async function openFindBar(tab) {
+async function openFindBar(tab, win = window) {
   info("Opening the find bar in the current tab.");
-  const findBar = await gBrowser.getFindBar(tab);
+  const findBar = await win.gBrowser.getFindBar(tab);
   const { promise, resolve } = Promise.withResolvers();
 
   findBar.addEventListener(
@@ -1612,6 +1612,23 @@ async function openFindBar(tab) {
   );
 
   findBar.open();
+  await promise;
+}
+
+async function closeFindBar(tab) {
+  info("Closing the find bar in the current tab.");
+  const findBar = await gBrowser.getFindBar(tab);
+  const { promise, resolve } = Promise.withResolvers();
+
+  findBar.addEventListener(
+    "findbarclose",
+    () => {
+      resolve();
+    },
+    { once: true }
+  );
+
+  findBar.close();
   await promise;
 }
 
